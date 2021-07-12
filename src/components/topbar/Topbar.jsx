93736502e60716dd18ab6d48ae88a7fc47d482cf
YS-RF <yanlin.song@sjsu.edu>
2021-07-12 08:57:52 -0700

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./topbar.css"
+import { Search, Person, Chat, Notifications } from "@material-ui/icons"
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">YS social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon"/>
+          <input placeholder="search for friend, post or video" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/profile/1.jpg" alt="" className="topbarImg" />
+      </div>
+    </div>
+    
+  )
+}
